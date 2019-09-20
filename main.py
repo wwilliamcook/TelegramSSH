cac0692aa6533239d97fa6f6d340fa6eed71e520
Weston Cook <wwilliamcook@gmail.com>
2019-09-20 01:22:39 -0700

    Consolidate arg_parser.py into main.py

--- main.py
+++ main.py
@@ -9,34 +9,37 @@ References:
 
 
 from telegram_bot import TelegramBot
-from arg_parser import parseArgs
 from get_token import getToken
+import argparse
 import logging
 
 
 admin_id = 314159265  # Insert your unique user ID here
 
 
-# Parse arguments
-a = parseArgs()
-
-
-def main(salt_path, token_path):
-    if a.verbose:
+def main(args):
+    if args.verbose:
         # Enable logging
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     
     # Get telegram token and get the corresponding bot
-    token = getToken(salt_path, token_path)
+    token = getToken(args.salt_path, args.token_path)
     print('Token successfully acquired, logging in...')
-    bot = TelegramBot(token, admin_id=admin_id, verbose=a.verbose)
+    bot = TelegramBot(token, admin_id=admin_id, verbose=args.verbose)
     print('Logged in. Bot started.')
     # Start bot
     bot.start()
     bot.idle()
-    if a.verbose:
+    if args.verbose:
         print('Bot stopped.')
 
 
 if __name__ == '__main__':
-    main('salt.pickle', 'token.pickle')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--salt_path', type=str, default='salt.pickle')
+    parser.add_argument('--token_path', type=str, default='token.pickle')
+    parser.add_argument('--verbose', '-v', action='store_true')
+    
+    args = parser.parse_args()
+    
+    main(args)
